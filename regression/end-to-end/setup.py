155ac4567dee9629ee40eb04ebf3aa5b67226d27
Hiruni Liyanage <87710665+the-liyanage@users.noreply.github.com>
2026-02-18 23:22:59 +0530

    Add setup file

---
+++ regression/end-to-end/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+name = "applied-machine-learning",
+version = "0.0.1",
+author = "H Liyanage",
+author_email="hiruniliyanage4@gmail.com",
+packages=find_packages(),
+install_requires = ["pandas", "numpy", "seaborn"]
+)
