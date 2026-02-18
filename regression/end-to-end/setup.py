b81280268ac4d3fa11bf324d0ee2e41f324939eb
Hiruni Liyanage <87710665+the-liyanage@users.noreply.github.com>
2026-02-18 23:52:58 +0530

    set.py and requirements added

--- regression/end-to-end/setup.py
+++ regression/end-to-end/setup.py
@@ -1,4 +1,21 @@
 from setuptools import find_packages,setup
+from typing import List
+
+hypen_e_dot = "-e ."
+
+def get_requirements(file_path:str)->List[str]:
+    # this function will return the list of requirements
+    requirements =[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if hypen_e_dot in requirements:
+            requirements.remove(hypen_e_dot)
+
+    return requirements
+
+
 
 setup(
 name = "applied-machine-learning",
@@ -6,5 +23,5 @@ version = "0.0.1",
 author = "H Liyanage",
 author_email="hiruniliyanage4@gmail.com",
 packages=find_packages(),
-install_requires = ["pandas", "numpy", "seaborn"]
+install_requires = get_requirements("requirements.txt")
 )
