38375e6b0c5dbae7e1a1a8e75dcdd7fd848c8b50
Hiruni Liyanage <87710665+the-liyanage@users.noreply.github.com>
2026-02-19 14:26:28 +0530

    logger and exception handling added

---
+++ regression/end-to-end/src/logger.py
@@ -0,0 +1,24 @@
+"""
+It's a diary for the ML project. Every single thing that happens — 
+loading data, training the model, errors, warnings — 
+gets written down automatically with a timestamp and line 
+number into a .log file.
+"""
+
+
+
+import logging 
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime("%m_%d_%Y_%H_%M_%S")}.log"
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+os.makedirs(logs_path, exist_ok = True)
+
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+logging.basicConfig(
+    filename = LOG_FILE_PATH, # Save logs here
+    format = "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level = logging.INFO,
+)
